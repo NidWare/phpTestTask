fbb410fa974d1690e89dfc74f8dba344ad8b1905
Terebbon <leonidsuhinin@MacBook-Pro-Leonid.local>
2022-01-14 13:48:45 +0300

    First commit

---
+++ main.php
@@ -0,0 +1,29 @@
+<?php
+
+    // Объявлем какой аргумент массива какому параметру отвечает
+
+    $method = 5;
+
+    $file = "access_log";
+    //$file = $argv[1];
+
+    if(!file_exists($file))exit('Файла не существует');
+
+    $file = file($file);
+
+    $views = count($file); // Получение количества просмотров
+
+
+
+    for ($i = 0; $i < $views; $i++) {
+        $string = $file[$i];
+        echo $string[0];
+    //    echo hasParam($string[$method], "GET");
+    }
+
+
+    function hasParam($f, $s) {
+        return substr_count($f, $s) > 0;
+    }
+
+?>
